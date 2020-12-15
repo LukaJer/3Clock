b999216f0d2b17c6e7a51ada429fe69aaf4106c8
luka <gumgum9000@gmail.com>
2020-12-15 23:59:17 +0100

    started on interrupts and added more structure

--- clock.c
+++ clock.c
@@ -8,45 +8,62 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "uart.h"
+#include <avr/interrupt.h>
 
-
+ISR(INT0_vect) //PPS
+{
+    // user code here
+    // Diff between GPS_Time and Internal_Time
+}
 
 double getTemp() //REads and calculates Temperature
 {
     //ADMUX = (ADMUX & ~(0x1F)) | (0 & 0x1F); //A0 as input Needed?
-    ADCSRA |= (1 << ADSC);                  // Read ADC
-    while (ADCSRA & (1 << ADSC))            // Convert and wait
+    ADCSRA |= (1 << ADSC);       // Read ADC
+    while (ADCSRA & (1 << ADSC)) // Convert and wait
     {
-    } 
+    }
     //TODO: ADC to Temp conversion
     /*
     ADCW*NTC_Coeff
     */
 
-   return 0; //
+    return 0; //
 }
 
 void initADC()
 {
     ADMUX = (1 << REFS1) | (0 << REFS0);                 // Set internal as Vref
-    ADMUX |= NTC_pin;                                        // Set A0 as input
+    ADMUX |= NTC_pin;                                    // Set A0 as input
     ADCSRA = (1 << ADPS1) | (1 << ADPS0) | (1 << ADPS0); //Frequenxy prescaler 128 (CLK/125kHz)
     ADCSRA |= (1 << ADEN);                               // Enable ADC
     ADCSRA |= (1 << ADSC);                               // Read ADC
     //Dummy Readout seems to be good practice
-    while (ADCSRA & (1 << ADSC))                         // Convert and wait
+    while (ADCSRA & (1 << ADSC)) // Convert and wait
     {
     }
-    (void)ADCW;                                          //Dump Dummy ADCW
+    (void)ADCW; //Dump Dummy ADCW
+}
+
+void GPS_Time(){
+
+}
+
+void Internal_Time(){
+
 }
 
 int main()
 {
+    // puts("String") for output
+    // char input = getchar(); for input
+
+    cli(); //Disable Interrupts
     uart_init();
     stdout = &uart_output;
-    stdin  = &uart_input;
-// puts("String") for output
-// char input = getchar(); for input
-
+    stdin = &uart_input;
+    PCICR = (1 << INT0);
+    EICRA = (1 << ISC00) | (1 << ISC01); //Rising Edge Intterupt
+    sei(); //Enable Interrupts
     return 0;
 }
