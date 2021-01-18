fddde1d2be7f8ade38a275c26213342badb4e0b7
Luka <gumgum9000@gmail.com>
2021-01-18 16:32:06 +0100

    first

--- clock.c
+++ clock.c
@@ -1,5 +1,5 @@
 #define F_CPU 16000000UL //Set CPU Clock to 16MHz
-#define BAUD 9600
+#define BAUD 115200
 #define NTC_pin PB0
 
 #include <avr/io.h>
@@ -26,28 +26,11 @@
 #define TOGGLE(x,y) (x ^= (1<<y)) //inverse
 
 uint32_t millis = 0;
+uint32_t millis_ISR = 0;
+uint32_t gps_millis = 0;
 uint32_t delta = 0;
-bool timer_running = false;
-
-bool IsGGA = false, Time_Set = false; //technicially GA,
-int GGA_Index;
-char GPS_Data[6];//HHMMSS
-char GPS_Buffer[3];
-char time[3]; //HMS
-
-void convTime(char * char_array, char * int_array)
-{
-	int i=0;
-    for(; i<3; i++){
-        int_array[i] = (char_array[i*2]-48)*10+char_array[i*2+1]-48;
-    }
-    return;
-}
-
-uint32_t time_to_millis(char * time)
-{
-	return (((uint32_t)time[0]*60+(uint32_t)time[1])*60+(uint32_t)time[2])*1000;
-}
+bool setup = true;
+int counter = 0;
 
 ISR(TIMER1_COMPA_vect)
 {
@@ -56,63 +39,24 @@ ISR(TIMER1_COMPA_vect)
 
 ISR(INT0_vect) //PPS
 {
-    // user code her
-    // Diff between GPS_Time and Internal_Time
-}
-
-ISR(USART_RX_vect) //GPS transmitts data
-{
-    char rec_char = UDR0;
-    //cli();
-    if (GGA_Index > 5) //Time data finished (we need 0..5)
-    {
-    	/*
-        if (!Time_Set) //Init Time
-        {
-            strncpy(time, GPS_Data, 8); //copies 8 chars from time to GPS_Data
-            Time_Set=true;
-        }
-        */
-        
-        GGA_Index = 0;
-        //printf("Time %.6s", GPS_Data);
-        convTime(GPS_Data, time);
-        printf("%d:%d:%d", time[0], time[1], time[2]);
-        
-        //Execute only when the first time data is received from the GPS
-        if(!timer_running){
-        millis = time_to_millis(time);
-		//TIMER: start timer with prescalar: 8
+	if(setup)
+	{
+		//start timer with prescalar: 8
 		SET(TCCR1B,CS11);
-		timer_running = true;
-		}
 		
-        printf(" delta_millis = %lu\n", (time_to_millis(time)-millis)-delta);
-        delta = (time_to_millis(time)-millis);
-        IsGGA = false;
-    }
-    
-    if (IsGGA) //checks for GA,
-    {
-        if(rec_char==',') IsGGA=false;
-        GPS_Data[GGA_Index] = rec_char; // write the received char into GPS_Data
-        GGA_Index++;
-    }
-    
-    else
-    {
-        GPS_Buffer[0] = GPS_Buffer[1];
-        GPS_Buffer[1] = GPS_Buffer[2];
-        GPS_Buffer[2] = rec_char;
-        if (GPS_Buffer[0] == 'G' && GPS_Buffer[1] == 'A' && GPS_Buffer[2] == ',')
-        {
-            IsGGA = true;
-            GGA_Index = 0;
-            GPS_Buffer[0] = 0;
-            GPS_Buffer[1] = 0;
-            GPS_Buffer[2] = 0;
-        }
-    }
+		//start timer with prescalar: 1
+		//SET(TCCR1B,CS10);
+		
+		setup = false;
+	}
+	else gps_millis = gps_millis + 1000;
+	
+	if(counter%30 == 0)
+	{
+		printf("%ld\n", (gps_millis - millis) - delta);
+		delta = gps_millis - millis;
+	}
+	counter++;
 }
 
 double getTemp() //Reads and calculates Temperature
@@ -148,43 +92,39 @@ void initADC()
     (void)ADCW; //Dump Dummy ADCW
 }
 
-const char *uart_getString(uint8_t length) //Reads String=Char[length] from UART; returns a pointer
-{
-    uint8_t charlength = 0;
-    char *uString = malloc(length);
-    do
-    {
-        uString[charlength] = getchar();
-        charlength++;
-    } while (uString[charlength - 1] != '\n' && charlength < length);
-    uString[length] = '\0';
-    return uString;
-}
-
-
-
 int main()
 {
     cli(); //Disable Interrupts
     uart_init();
     stdout = &uart_output;
     stdin = &uart_input;
-    PCICR = (1 << INT0);
-    EICRA = (1 << ISC00) | (1 << ISC01); //Rising Edge Intterupt
+    
+    //SET EXTERNAL INTERRUPT
+    //
+   	//Rising Edge Intterupt
+    SET(EICRA,ISC00);
+    SET(EICRA,ISC01);
+    //
+    //activate INTO external interrupt
+    SET(EIMSK,INT0);
+    
+    
     puts("Hello World!");
     _delay_ms(10);
     
     
-    //TIMER: Set CTC Bit, so counter will auto-restart, when it compares true to the timervalue
+    //timer config
+    //
+    //Set CTC Bit, so counter will auto-restart, when it compares true to the timervalue
 	SET(TCCR1B,WGM12);
-	
-	//TIMER: 16-Bit Value continuesly compared to counter register
+	//
+	//16-Bit Value continuesly compared to counter register
 	OCR1A = 2000;
-	
-	//TIMER: Timer/Counter Interrupt Mask Register has to be set to 1 at OCIE0A, so the interrupt will not be masked
+	//
+	//Timer/Counter Interrupt Mask Register has to be set to 1 at OCIE0A, so the interrupt will not be masked
 	SET(TIMSK1,OCIE1A);
-	
-	//TIMER: enable interrupts
+	//
+	//enable interrupts
 	sei();
 	
 
@@ -194,21 +134,3 @@ int main()
     }
     return 0;
 }
-
-//UART Manual
-/*
-Input:
-char *input = uart_getString(length of String);
-//do something with input
-free(input);
-
-Output:
-puts() or printf()
-*/
-
-//GPSData
-/*
-$GPRMC,235316.000,A,4003.9040,N,10512.5792,W,0.09,144.75,141112,,*19
-$GPGGA,235317.000,4003.9039,N,10512.5793,W,1,08,1.6,1577.9,M,-20.7,M,,0000*5F
-$GPGSA,A,3,22,18,21,06,03,09,24,15,,,,,2.5,1.6,1.9*3E
-*/
